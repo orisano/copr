901e0d59c02b2b018bf5c3195d4a70baffd53069
Nao YONASHIRO <owan.orisano@gmail.com>
2017-10-08 22:16:55 +0900

    feat: LCA<O(n), O(1)>の追加

---
+++ graph/lca.hpp
@@ -0,0 +1,16 @@
+#include <vector>
+
+struct LCA {
+  EulerTour& euler_tour;
+  const RMQ<int> rmq;
+
+  LCA(EulerTour& euler_tour) : euler_tour(euler_tour), rmq(euler_tour.depth) {}
+
+  int query(int u, int v) const {
+    int l = euler_tour.ord[u];
+    int r = euler_tour.ord[v];
+    if (r < l) std::swap(r, l);
+    int x = rmq.query(l, r);
+    return euler_tour.id[x];
+  }
+};
