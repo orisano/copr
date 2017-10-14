810c187dec4f447a6b319ff36523b086167e3187
Nao YONASHIRO <owan.orisano@gmail.com>
2017-10-15 03:14:26 +0900

    test: StarrySkyTreeをstarry skyでverify
    http://joisc2009.contest.atcoder.jp/submissions/1686056

--- verify/atcoder/joisc2009/joisc2009_starry_sky.cpp
+++ verify/atcoder/joisc2009/joisc2009_starry_sky.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdio>
+#include <queue>
 #include <vector>
 
 #define mygc(c) (c) = getchar_unlocked()
@@ -10,18 +11,19 @@
 template<typename T=int>inline T rd(){T x=0,m=0,k;for(;;){mygc(k);if(k=='-'){m=1;break;}if('0'<=k&&k<='9'){x=k-'0';break;}}for(;;){mygc(k);if(k<'0'||'9'<k)break;x=x*10+k-'0';}return x;}
 template<typename T=int>inline void wr(T x,char c='\n'){int s=0,m=0;char b[32];if(x<0)m=1,x=-x;for(;x;x/=10)b[s++]=x%10;if(!s)b[s++]=0;if(m)mypc('-');for(;s--;)mypc(b[s]+'0');mypc(c);}
 
+
 struct Star {
   int x, y, L;
+  short l, po;
   Star(int x, int y, int L) : x(x), y(y), L(L) {}
   bool operator<(const Star& rhs) const {
-    if (L != rhs.L) return L > rhs.L;
-    if (x != rhs.x) return x > rhs.x;
-    return y > rhs.y;
+    return y < rhs.y;
   }
   inline bool ok(const Star& o) const {
-    return ((y > o.y) ? (y - o.y) : (o.y - y)) <= L && ((x > o.x) ? (x - o.x) : (o.x - x)) <= L;
+    return L <= o.L && std::abs(y - o.y) <= L && std::abs(x - o.x) <= L;
   }
 };
+
 
 int main() {
   int N = rd();
@@ -39,36 +41,48 @@ int main() {
   std::sort(xs.begin(), xs.end());
   xs.erase(std::unique(xs.begin(), xs.end()), xs.end());
 
+
   std::sort(stars.begin(), stars.end());
+  for (auto& si : stars) {
+    si.l = std::lower_bound(xs.begin(), xs.end(), si.x) - xs.begin();
+    short po = 1;
+    for (auto& sj : stars) {
+      if (si.ok(sj)) po++;
+    }
+    si.po = po;
+  }
 
   StarrySkyTree<short> sst(4000);
   short maxi = 0;
 
   struct Event {
-    int t, l, r, v;
-    Event(int t, int l, int r, int v) : t(t), l(l), r(r), v(v) {}
-    bool operator<(const Event& rhs) const { return t < rhs.t; }
+    int y;
+    short l, r;
+    Event(int y, short l, short r) : y(y), l(l), r(r) {}
   };
-  std::vector<Event> es;
-  es.reserve(N * 2);
-  for (int i = N - 1; i >= 0; i--) {
-    const auto& si = stars[i];
-    es.clear();
-    for (int j = 0; j <= i; j++) {
-      const auto& sj = stars[j];
-      if (not si.ok(sj)) continue;
-      int l = std::lower_bound(xs.begin(), xs.end(), sj.x) - xs.begin();
-      int r = std::lower_bound(xs.begin(), xs.end(), sj.x + si.L + 1) - xs.begin();
-      es.emplace_back(sj.y, l, r, 1);
-      es.emplace_back(sj.y + si.L + 1, l, r, -1);
-    }
-    if (es.size() / 2 < maxi) continue;
 
-    std::sort(es.begin(), es.end());
-    for (const auto& e : es) {
-      sst.add(e.v, e.l, e.r);
+  for (auto&& si : stars) {
+    if (si.po <= maxi) continue;
+
+    std::queue<Event> es;
+    for (auto&& sj : stars) {
+      if (not si.ok(sj)) continue;
+      while (!es.empty() && es.front().y < sj.y) {
+        auto e = es.front();
+        es.pop();
+        sst.add(-1, e.l, e.r);
+      }
+      int r = std::lower_bound(xs.begin(), xs.end(), sj.x + si.L + 1) - xs.begin();
+      es.emplace(sj.y + si.L + 1, sj.l, r);
+
+      sst.add(1, sj.l, r);
       short v = sst.query(0, sst.N);
       if (maxi < v) maxi = v;
+    }
+    while (!es.empty()) {
+      auto e = es.front();
+      es.pop();
+      sst.add(-1, e.l, e.r);
     }
   }
   wr(maxi);
