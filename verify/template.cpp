a334af78c8d8769642d956576f5425aff1546957
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-20 00:51:44 +0900

    chore: templateがclang-formatされない様に変更

--- verify/template.cpp
+++ verify/template.cpp
@@ -1,8 +1,10 @@
 #include <cstdio>
 #define mygc(c) (c) = getchar_unlocked()
 #define mypc(c) putchar_unlocked(c)
+// clang-format off
 template<typename T=int>inline T rd(){T x=0,m=0,k;for(;;){mygc(k);if(k=='-'){m=1;break;}if('0'<=k&&k<='9'){x=k-'0';break;}}for(;;){mygc(k);if(k<'0'||'9'<k)break;x=x*10+k-'0';}if(m)x=-x;return x;}
 template<typename T=int>inline void wr(T x,char c='\n'){int s=0,m=0;char b[32];if(x<0)m=1,x=-x;for(;x;x/=10)b[s++]=x%10;if(!s)b[s++]=0;if(m)mypc('-');for(;s--;)mypc(b[s]+'0');mypc(c);}
+// clang-format on
 
 int main() {
   return 0;
