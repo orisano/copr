989ed4a8d8b0288bba6ede07fbdeae1f9a0dc86b
Nao YONASHIRO <owan.orisano@gmail.com>
2017-11-21 04:06:03 +0900

    feat: evalに渡す引数を区間の長さではなく区間に変更

--- include/copr/sequence/lazy_seg_tree.hpp
+++ include/copr/sequence/lazy_seg_tree.hpp
@@ -17,45 +17,58 @@ struct LazySegTree {
   LazySegTree(int size, int h, int aligned)
       : N(aligned), H(h), nodes(aligned * 2, Monoid::empty()), lazy(aligned, none) {}
 
-  inline void apply(int p, lazy_type value, int k) {
-    nodes[p] = Op::eval(nodes[p], value, k);
+  inline void apply(int p, lazy_type value, int l, int r) {
+    nodes[p] = Op::eval(nodes[p], value, l, r);
     if (p < N) lazy[p] = Op::merge(lazy[p], value);
   }
-  inline void calc(int p, int k) {
+  inline void calc(int p, int l, int r) {
     auto x = Monoid::append(nodes[p << 1], nodes[p << 1 | 1]);
     if (lazy[p] == none) {
       nodes[p] = x;
     } else {
-      nodes[p] = Op::eval(x, lazy[p], k);
+      nodes[p] = Op::eval(x, lazy[p], l, r);
     }
   }
   void build(Index0 l, Index0 r) {
-    int k = 2;
-    for (l += N, r += N - 1; l > 1; k <<= 1) {
-      l >>= 1, r >>= 1;
-      for (int i = r; i >= l; --i) calc(i, k);
+    int k = 1;
+    int R = r;
+    for (l += N, r += N - 1; l > 1;) {
+      if (!(r & 1)) R += k;
+      l >>= 1, r >>= 1, k <<= 1;
+      for (int i = r, j = R; i >= l; --i, j -= k) calc(i, j - k, j);
     }
   }
   void push(Index0 l, Index0 r) {
     int s = H, k = 1 << (H - 1);
-    for (l += N, r += N - 1; s > 0; --s, k >>= 1) {
-      for (int i = l >> s; i <= r >> s; ++i) {
+    int L = 0;
+    for (l += N, r += N - 1; s > 0; k >>= 1) {
+      for (int i = l >> s, j = L; i <= r >> s; ++i, j += k) {
+        j += k;
         if (lazy[i] != none) {
-          apply(i << 1, lazy[i], k);
-          apply(i << 1 | 1, lazy[i], k);
+          apply(i << 1, lazy[i], j - k, j);
+          apply(i << 1 | 1, lazy[i], j, j + k);
           lazy[i] = none;
         }
       }
+      --s;
+      if ((l >> s) & 1) L += k;
     }
   }
   void exec(Index0 l, Index0 r, lazy_type value) {
     if (value == none) return;
     int L = l, R = r, k = 1;
+    int i = l, j = r;
     push(L, L + 1);
     push(R - 1, R);
     for (l += N, r += N; l < r; l >>= 1, r >>= 1, k <<= 1) {
-      if (l & 1) apply(l++, value, k);
-      if (r & 1) apply(--r, value, k);
+      if (l & 1) {
+        apply(l++, value, i, i + k);
+        i += k;
+      }
+      if (r & 1) {
+        apply(--r, value, j - k, j);
+        j -= k;
+      }
     }
     build(L, L + 1);
     build(R - 1, R);
