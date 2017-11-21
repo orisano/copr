248549912dc3f287344ffef112bfb48f0383cc5c
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-11-21 16:55:47 +0900

    wip

--- include/copr/graph/hl_decomposition.hpp
+++ include/copr/graph/hl_decomposition.hpp
@@ -1,90 +1,86 @@
 #pragma once
 #include <algorithm>
+#include <cstdlib>
 #include <functional>
 #include <vector>
 
 struct HLDecomposition {
-  struct Node {
-    int ord, par, depth;
-    int idx;
-    int path_id = -1;
-    Node() {}
-  };
-
   const int N;
-  std::vector<std::vector<int>> tree, pathes;
-  std::vector<Node> nodes;
-  std::vector<int> Q;
+  std::vector<std::vector<int>> tree;
 
-  HLDecomposition(int N) : N(N), tree(N), nodes(N) {}
+  std::vector<int> cluster, par, depth, ord;
+  std::vector<int> head, offset;
+
+  // ord[head[cluster[u]] + offset[u]] == u
+
+  HLDecomposition(int N) : N(N), tree(N), cluster(N, -1), par(N, -1), depth(N), ord(N), offset(N) {}
+
   void add_edge(int u, int v) {
     tree[u].push_back(v);
     tree[v].push_back(u);
   }
   void build(int root = 0) {
+    std::vector<int> Q;
     Q.reserve(N);
     Q.push_back(root);
-    nodes[root].par = -1;
-    nodes[root].depth = 0;
-    for (int i = 0; i < Q.size(); i++) {
+    for (int i = 0; i < N; i++) {
       int u = Q[i];
-      auto& U = nodes[u];
-      U.ord = i;
       for (int v : tree[u]) {
-        if (U.par == v) continue;
-        auto& V = nodes[v];
-        V.par = u;
-        V.depth = U.depth + 1;
+        if (par[u] == v) continue;
+        par[v] = u;
+        depth[v] = depth[u] + 1;
         Q.push_back(v);
       }
     }
-    decomposition();
+    std::vector<int> subtree_size(N, 1);
+    for (int i = N - 1; i > 0; i--) {
+      subtree_size[par[Q[i]]] += subtree_size[Q[i]];
+    }
+
+    std::vector<std::vector<int>> pathes;
+    for (int u : Q) {
+      if (cluster[u] == -1) {
+        cluster[u] = pathes.size();
+        pathes.emplace_back();
+      }
+      pathes[cluster[u]].push_back(u);
+
+      int max_subsize = -1, selected = -1;
+      for (int v : tree[u]) {
+        if (par[u] == v) continue;
+        if (max_subsize >= subtree_size[v]) continue;
+        max_subsize = subtree_size[v];
+        selected = v;
+      }
+      if (selected != -1) cluster[selected] = u;
+    }
+
+    int P = pathes.size();
+    head.resize(P + 1);
+    for (int p = 0; p < P; p++) {
+      int H = head[p];
+      int L = pathes[p];
+      head[p + 1] = H + L;
+      for (int i = 0; i < L; i++) {
+        int v = pathes[p][i];
+        offset[v] = i;
+        ord[H + i] = v;
+      }
+    }
   }
   void for_each(int u, int v, std::function<void(int, int, int)> f) const {
-    while (nodes[u].path_id != nodes[v].path_id) {
-      int head_u = pathes[nodes[u].path_id][0];
-      int head_v = pathes[nodes[v].path_id][0];
-      if (nodes[head_u].depth > nodes[head_v].depth) {
-        std::swap(u, v);
-        std::swap(head_u, head_v);
-      }
-      f(nodes[v].path_id, 0, nodes[v].idx + 1);
-      v = nodes[head_v].par;
+    while (cluster[u] != cluster[v]) {
+      if (depth[head[cluster[u]]] > depth[head[cluster[v]]]) std::swap(u, v);
+      int c = cluster[v];
+      f(c, head[c], head[c] + offset[v] + 1);
+      v = par[head[c]];
     }
-    if (nodes[v].idx < nodes[u].idx) std::swap(u, v);
-    f(nodes[v].path_id, nodes[u].idx, nodes[v].idx + 1);
+    if (offset[v] < offset[u]) std::swap(u, v);
+    f(cluster[u], head[cluster[u]] + offset[u], head[cluster[v]] + offset[v] + 1);
   }
   int lca(int u, int v) const {
     int x;
     for_each(u, v, [&](int p, int l, int r) { x = pathes[p][l]; });
     return x;
-  }
-  void decomposition() {
-    std::vector<int> subtree_size(N, 1);
-    for (int i = N - 1; i > 0; i--) {
-      subtree_size[nodes[Q[i]].par] += subtree_size[Q[i]];
-    }
-    for (int u : Q) {
-      auto& U = nodes[u];
-      if (U.path_id == -1) {
-        U.path_id = pathes.size();
-        pathes.emplace_back();
-      }
-      pathes[U.path_id].push_back(u);
-
-      int max_subsize = -1, selected = -1;
-      for (int v : tree[u]) {
-        if (U.par == v) continue;
-        if (max_subsize >= subtree_size[v]) continue;
-        max_subsize = subtree_size[v];
-        selected = v;
-      }
-      if (selected != -1) nodes[selected].path_id = U.path_id;
-    }
-    for (auto&& path : pathes) {
-      for (int i = 0; i < path.size(); i++) {
-        nodes[path[i]].idx = i;
-      }
-    }
   }
 };
